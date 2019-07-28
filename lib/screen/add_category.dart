01771b3606bbe239056fd81c91fd0b5f22f5ab27
Mujib <mujib517@gmail.com>
2019-07-28 16:04:01 +0530

    Radiobuttons in add category widget

--- lib/screen/add_category.dart
+++ lib/screen/add_category.dart
@@ -11,10 +11,35 @@ class AddCategory extends StatefulWidget {
 
 class AddCategoryState extends State<AddCategory> {
   EntryType type;
+  int selected = 0;
+  final icons = [0xe57a, 0xeb44, 0xe850, 0xe88a];
 
   AddCategoryState(this.type);
 
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Add Category")));
+    return Scaffold(
+      appBar: AppBar(title: Text("Add Category")),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return ListView(children: _getIcons());
+  }
+
+  List<Widget> _getIcons() {
+    return icons
+        .map((int icon) => ListTile(
+              leading: Icon(IconData(icon, fontFamily: "MaterialIcons")),
+              title: Radio(
+                  value: icon,
+                  groupValue: selected,
+                  onChanged: (int index) {
+                    setState() {
+                      this.selected = icons[index];
+                    }
+                  }),
+            ))
+        .toList();
   }
 }
